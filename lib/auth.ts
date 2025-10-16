d1f04187e3e29662eb20df4f6210cb27c8f4dedf
szbr1 <szb8480@gmail.com>
2025-10-16 13:21:28 +0500

    Integrated MailerSend with improved authentication for verification emails

--- lib/auth.ts
+++ lib/auth.ts
@@ -2,11 +2,20 @@ import db from "@/utils/db"
 import { betterAuth } from "better-auth"
 import { nextCookies } from "better-auth/next-js"
 import { mongodbAdapter } from "better-auth/adapters/mongodb";
+import { sendEmailVerification } from "@/components/features/verification-email";
 
 export const auth = betterAuth({
     plugins: [nextCookies()],
     emailAndPassword: {    
-        enabled: true
+        enabled: true,
+        requireEmailVerification: true
+    },
+    emailVerification: {
+        autoSignInAfterVerification: true,
+        sendOnSignUp: true,
+        sendVerificationEmail: async ({user, url})=> {
+            await sendEmailVerification(user, url)
+        }
     },
     database: mongodbAdapter(db)
 })
